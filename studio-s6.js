81ca21cface7eb624a55667b088623d2ca65e205
Source Academy User <Marcus Soh>
2022-09-13 14:54:40 +0800

    Changes made from Source Academy

--- studio-s6.js
+++ studio-s6.js
@@ -1,31 +1,50 @@
-// // Question 1
+// Question 1
 // function my_map(f, xs) {
-//     return accumulate((x, y) => append(list(f(x)), y), list(), xs);
+//     // return accumulate((x, y) => append(list(f(x)), y), list(), xs);
+//     return accumulate((x, y) => pair((f(x)), y), null, xs);
 // }
 
 // my_map(x => x + 1, list(1,2,3));
 
 // // Question 2
-// function check_if_exists(element, to_compare) {
-//     // filter the to_compare
-//     return length(filter(e => e === element, to_compare)) !== 0; // true if exists
-// }
-// function remove_duplicates(lst) {
-//     // iterate the list
-//     // add any seen ones to the new list
-//     const iter = (lst, seen) => {
-//         return is_null(lst) 
-//             ? seen
-//             : check_if_exists(head(lst), seen)
-//             ? iter(tail(lst), seen)
-//             : iter(tail(lst), append(seen, list(head(lst))));
-//     };
+function check_if_exists(element, to_compare) {
+    // filter the to_compare
+    return length(filter(e => e === element, to_compare)) !== 0; // true if exists
     
-//     return iter(lst, list());
-// }
+}
+function remove_duplicates(lst) {
+    // iterate the list
+    // add any seen ones to the new list
+    // const iter = (lst, seen) => {
+    //     return is_null(lst) 
+    //         ? seen
+    //         : check_if_exists(head(lst), seen)
+    //         ? iter(tail(lst), seen)
+    //         : iter(tail(lst), append(seen, list(head(lst))));
+    // };
+    
+    // return iter(lst, list());
+    
+    
+    return accumulate((x, y) => {
+        return check_if_exists(x, y) 
+            ? y
+            : append(y, list(x));
+    }, list(), lst);
+    // const iter = (lst, seen) => {
+    //     return is_null(lst) 
+    //         ? seen
+    //         : check_if_exists(head(lst), seen)
+    //         ? iter(tail(lst), seen)
+    //         : iter(tail(lst), pair(seen, (head(lst))));
+    // };
+    
+    // return iter(lst, null);
+}
 
-// display_list(remove_duplicates(list(1, 2, 3, 4, 4, 3, 2, 1, 2)));
-// display_list(remove_duplicates(list("a", "x", "b", "c", "c", "b", "d")));
+
+display_list(remove_duplicates(list(1, 2, 3, 4, 4, 3, 2, 1, 2)));
+display_list(remove_duplicates(list("a", "x", "b", "c", "c", "b", "d")));
 
 
 function makeup_amount(x, coins) {
@@ -35,12 +54,13 @@ function makeup_amount(x, coins) {
         return null;
     } else {
         // Combinations that do not use the head coin.
-        const combi_A = append(list(), makeup_amount(x, tail(coins)));
+        const combi_A = makeup_amount(x, tail(coins));
+  
         // Combinations that do not use the head coin
         // for the remaining amount.
-        // const combi_B = ...
+        const combi_B = makeup_amount(x - head(coins), tail(coins));
         // Combinations that use the head coin.
-        const combi_C = append(list(head(coins)), makeup_amount(x - head(coins), tail(coins)));
+        const combi_C = map(lst => pair(head(coins), lst) , combi_B);
         return append(combi_A, combi_C);
     }
 }
