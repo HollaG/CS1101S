d92c55b128a289f53920c9096c3ad9c1ab6a985d
Marcus Soh <No public email provided>
2022-11-07 15:47:21 +0800

    Changes made from Source Academy

--- pe1-1819-q2.js
+++ pe1-1819-q2.js
@@ -1,4 +1,4 @@
-// QUESTION 3
+// QUESTION 2
 
 // Instructions for students who are using this for practice:
 //
@@ -50,254 +50,233 @@ function assert(test_name, test_func, truth, dependence) {
 
 
 //===============================================================
-// TASK 3A(I)
+// DO NOT REMOVE OR MODIFY THE FOLLOWING FUNCTIONS.
+// You may call them in your functions.
 //===============================================================
-function count_lower_neighbors(emap, r, c) {
-
-    // WRITE HERE.
-    // r must be > 1 and < # rows - 1
-    // c must be > 1 and < # cols - 1
-    
-    const rows_num = array_length(emap);
-    const cols_num = array_length(emap[0]);
-    
-    if ((r > 0 && r < rows_num - 1) && (c > 0 && c < cols_num - 1)) {
-        // valid
-        let count = 0;
-        let elem = emap[r][c];
-        // check row - 1: col-1, col, col+1
-        if (emap[r-1][c-1] < elem) {
-            count = count + 1;
-        }
-        if (emap[r-1][c] < elem) {
-            count = count + 1;
-        }
-        if (emap[r-1][c+1] < elem) {
-            count = count + 1;
-        }
-        
-        // check row: col-1, col+1
-        if (emap[r][c-1] < elem) {
-            count = count + 1;
-        }
-        if (emap[r][c+1] < elem) {
-            count = count + 1;
-        }
-       
-        // check row + 1: col-1, col, col+1
-        if (emap[r+1][c-1] < elem) {
-            count = count + 1;
-        }
-        if (emap[r+1][c] < elem) {
-            count = count + 1;
-        }
-        if (emap[r+1][c+1] < elem) {
-            count = count + 1;
-        }
-        return count;
-        
-    } else {
-        // invalid
-        return 0;
+function swap(A, i, j) {
+    const temp = A[i];
+    A[i] = A[j];
+    A[j] = temp;
+}
+//---------------------------------------------------------------
+function copy_array(A) {
+    const len = array_length(A);
+    const B = [];
+    for (let i = 0; i < len; i = i + 1) {
+        B[i] = A[i];
     }
+    return B;
+}
+//---------------------------------------------------------------
+function reverse_array(A) {
+    const len = array_length(A);
+    const half_len = math_floor(len / 2);
+    for (let i = 0; i < half_len; i = i + 1) {
+        swap(A, i, len - 1 - i);
+    }
+}
+//---------------------------------------------------------------
+function array_to_list(A) {
+    const len = array_length(A);
+    let L = null;
+    for (let i = len - 1; i >= 0; i = i - 1) {
+        L = pair(A[i], L);
+    }
+    return L;
+}
+//---------------------------------------------------------------
+function list_to_array(L) {
+    const A = [];
+    let i = 0;
+    for (let p = L; !is_null(p); p = tail(p)) {
+        A[i] = head(p);
+        i = i + 1;
+    }
+    return A;
+}
+//---------------------------------------------------------------
+// Sorts the array of numbers in ascending order.
+function sort_ascending(A) {
+    const len = array_length(A);
+    for (let i = 1; i < len; i = i + 1) {
+        const x = A[i];
+        let j = i - 1;
+        while (j >= 0 && A[j] > x) {
+            A[j + 1] = A[j];
+            j = j - 1;
+        }
+        A[j + 1] = x;
+    }
+}
+//---------------------------------------------------------------
+function digits_to_string(digits) {
+    const len = array_length(digits);
+    let str = "";
+    for (let i = 0; i < len; i = i + 1) {
+        str = str + stringify(digits[i]);
+    }
+    return str;
+}
+// const D = [8, 3, 9, 2, 8, 1];
+// digits_to_string(D);  // returns "839281"
+//===============================================================
+// DO NOT REMOVE OR MODIFY THE ABOVE FUNCTIONS.
+//===============================================================
 
 
+
+//===============================================================
+// TASK 2A
+//===============================================================
+function build_largest_int(digits) {
+    
+    
+    // WRITE HERE.
+    // copy the array
+    let copy = [];
+    for (let i = 0; i < array_length(digits); i = i + 1) {
+        copy[i] = digits[i];
+    }
+    
+   
+    sort_ascending(copy);
+    let final = reverse_array(copy);
+    let res = "";
+    for (let i = 0; i < array_length(copy); i = i + 1) {
+        res = res + stringify(copy[i]);
+    }
+    display(res);
+    return res;
 }
 
 
-// TASK 3A(I) TESTS
-const emapA1 =
-[[3, 1, 1, 1, 1, 1, 1],
- [1, 1, 1, 1, 2, 3, 1],
- [1, 0, 3, 2, 1, 1, 0],
- [1, 1, 1, 1, 3, 1, 1],
- [1, 2, 1, 1, 3, 1, 3],
- [1, 1, 1, 1, 4, 1, 1]];
-assert("3A(I)_1", () => count_lower_neighbors([[5]], 0, 0), 0, []);
-assert("3A(I)_2", () => count_lower_neighbors(emapA1, 0, 0), 0, []);
-assert("3A(I)_3", () => count_lower_neighbors(emapA1, 5, 4), 0, []);
-assert("3A(I)_4", () => count_lower_neighbors(emapA1, 4, 6), 0, []);
-assert("3A(I)_5", () => count_lower_neighbors(emapA1, 1, 1), 1, []);
-assert("3A(I)_6", () => count_lower_neighbors(emapA1, 2, 2), 8, []);
-assert("3A(I)_7", () => count_lower_neighbors(emapA1, 2, 3), 5, []);
-assert("3A(I)_8", () => count_lower_neighbors(emapA1, 4, 4), 6, []);
+// TASK 2A TESTS
+assert("2A_1", () => build_largest_int([1]),
+    "1", []);
+assert("2A_2", () => build_largest_int([1,2,3,4,5]),
+    "54321", []);
+assert("2A_3", () => build_largest_int([9,8,7]),
+    "987", []);
+assert("2A_4", () => build_largest_int([4,1,9,1,4,9,1]),
+    "9944111", []);
+assert("2A_5", () => build_largest_int([5,5,5,5,5,5,7,5,5,5]),
+    "7555555555", []);
+assert("2A_6", () => build_largest_int([5,5,5,5,5,5,5,5,5,5]),
+    "5555555555", []);
 
 
 //===============================================================
-// TASK 3A(II)
+// TASK 2B
 //===============================================================
-function count_peaks(emap) {
+// 98765
+// 98756
+
+// 98766
+// 98676
+
+function build_2nd_largest_int(digits) {
 
     // WRITE HERE.
-    const rows_num = array_length(emap);
-    const cols_num = array_length(emap[0]);
-    let count = 0;
-    for (let r = 0; r < rows_num; r = r + 1) {
-        for (let c = 0; c < cols_num; c = c + 1) {
-            if (count_lower_neighbors(emap, r, c) === 8) {
-                count = count + 1;
-            }
-        }
+    // idea: make the largest int
+    // then: keep the 1st (i = 0) element, 
+    // and apply a function to sort the remaining ones
+    
+    
+    // swap 1 and 10
+    // if same, swap 10 and 100
+    
+    let copy = [];
+    for (let i = 0; i < array_length(digits); i = i + 1) {
+        copy[i] = digits[i];
     }
-
-    return count;
-
-}
-
-
-// TASK 3A(II) TESTS
-const emapA2a =
-[[3, 1, 1, 1, 1, 1, 1],
- [1, 1, 1, 1, 2, 3, 1],
- [1, 0, 3, 2, 1, 1, 0],
- [1, 1, 1, 1, 3, 1, 1],
- [1, 2, 1, 1, 3, 1, 3],
- [1, 1, 1, 1, 4, 1, 1]]; // 3 peaks
-const emapA2b =
-[[3, 1, 4, 1, 5, 1, 6, 1],
- [1, 1, 1, 1, 1, 1, 1, 1],
- [1, 7, 1, 8, 1, 9, 1, 0],
- [1, 1, 1, 1, 1, 1, 1, 1],
- [2, 1, 3, 1, 4, 1, 5, 2],
- [1, 1, 1, 1, 1, 1, 1, 1],
- [1, 9, 1, 8, 1, 7, 1, 6],
- [1, 1, 1, 1, 1, 1, 1, 1],
- [8, 1, 9, 1, 8, 1, 9, 1]]; // 9 peaks
-assert("3A(II)_1", () => count_peaks([[5]]),
-    0, ["count_lower_neighbors"]);
-assert("3A(II)_2", () => count_peaks([[2,3,4],[3,5,3],[4,3,2]]),
-    1, ["count_lower_neighbors"]);
-assert("3A(II)_3", () => count_peaks(emapA2a),
-    3, ["count_lower_neighbors"]);
-assert("3A(II)_4", () => count_peaks(emapA2b),
-    9, ["count_lower_neighbors"]);
-
-
-//===============================================================
-// TASK 3B
-//===============================================================
-function count_islands(emap) {
-
-    // WRITE HERE.
-    let final_list = list(); 
     
-    let visited = list();
-    
-    const rows_num = array_length(emap);
-    const cols_num = array_length(emap[0]);
-    
-    function helper(r, c) {
-        if (r < 0 || c < 0 || r > rows_num - 1 || c > cols_num - 1) {
-            return 0;
-        }
-        
-        if (emap[r][c] === 0) {
-            return 0;
-        }
-        
-        if (check_visited(r, c)) {
-            // visited, stop checking
-            return 0;
+   
+    sort_ascending(copy);
+    // display_list(copy);
+    reverse_array(copy);
+    // display_list(copy);
+    let last_digit = copy[array_length(copy)-1];
+    for (let i = array_length(copy) - 1; i >= 0; i = i - 1) {
+        if (i === 0) {
+            
         } else {
-            // display(list(r, c), "found: ");
-            visited = append(visited, list(list(r, c)));
-        }
-        
-        // visit above 
-        let r_above = r - 1;
-        let c_above = c;
-        helper(r_above, c_above);
-        
-        // visit left
-        let r_left = r;
-        let c_left = c - 1;
-        helper(r_left, c_left);
-        // visit right
-        let r_right = r;
-        let c_right = c + 1;
-        helper(r_right, c_right);
-        
-        // visit down
-        let r_down = r + 1;
-        let c_down = c;
-        helper(r_down, c_down);
-        
-    }
-    
-    function check_visited(r, c) {
-        let has_visited = false;
-        for_each(e => {
-            if (head(e) === r && head(tail(e)) === c) {
-                has_visited = true;
-            }
-        }, visited);
-        
-        return has_visited;
-        
-        
-    }
-    
-    let islands = 0;
-    for (let r = 0; r < rows_num; r = r + 1) {
-        for (let c = 0; c < cols_num; c = c + 1) {
-            if (!check_visited(r, c)) {
-                // not visited before
-               
-               const val = emap[r][c];
-            
-                if (val === 0) {
-                    // is water
-                } else {
-                    // is NOT water, check above and below
-                    
-                    islands = islands + 1;
-                  
-                    // helper vists all the connected islands and adds it to visited
-                    helper(r, c);
-                    
-                    visited = append(visited, list(list(r, c)));
-                
-                }
+            if (copy[i] !== copy[i-1]) {
+                // swap
+                swap(copy, i, i-1);
+                break;
             } else {
+               // don't swap yet
                 
-                continue;
+               
             }
-            
-            
-           
-            
         }
     }
-    // display_list(visited);
-    display(islands, "islands");
-    return islands;
+    
+    
+
+    
+    let str = "";
+    for (let i = 0; i < array_length(copy); i = i + 1) {
+        str = str + stringify(copy[i]);
+    }
+    return str;
+    
+    
+    
+  
+    
+
 }
 
 
-// TASK 3B TESTS
-const emapB1 =
-[[2, 1, 0, 2, 1, 1, 3],
- [0, 1, 0, 1, 0, 0, 2],
- [0, 0, 0, 2, 3, 1, 1],
- [1, 0, 2, 0, 0, 0, 0],
- [0, 0, 1, 2, 0, 0, 0],
- [1, 0, 3, 0, 1, 1, 2]]; // 6 islands
-const emapB2 =
-[[1, 2, 0, 0, 1, 0, 0, 1],
- [1, 2, 2, 3, 1, 0, 2, 1],
- [0, 1, 1, 0, 1, 0, 0, 1],
- [0, 0, 0, 0, 0, 3, 3, 0],
- [1, 1, 2, 0, 0, 0, 0, 0],
- [1, 0, 1, 0, 0, 1, 2, 3],
- [1, 3, 2, 1, 1, 0, 1, 1]]; // 5 islands
-assert("3B_1", () => count_islands([[0]]), 0, []);
-assert("3B_2", () => count_islands([[1]]), 1, []);
-assert("3B_3", () => count_islands([[0,0], [0,0]]), 0, []);
-assert("3B_4", () => count_islands([[2,1], [1,3]]), 1, []);
-assert("3B_5", () => count_islands([[0,1], [0,0]]), 1, []);
-assert("3B_6", () => count_islands([[2,0], [0,1]]), 2, []);
-assert("3B_7", () => count_islands(emapB1), 6, []);
-assert("3B_8", () => count_islands(emapB2), 5, []);
+// TASK 2B TESTS
+assert("2B_1", () => build_2nd_largest_int([1]),
+    "1", ["build_largest_int"]);
+assert("2B_2", () => build_2nd_largest_int([1,2,3,4,5]),
+    "54312", ["build_largest_int"]);
+assert("2B_3", () => build_2nd_largest_int([9,8,7]),
+    "978", ["build_largest_int"]);
+assert("2B_4", () => build_2nd_largest_int([4,1,9,1,4,9,1]),
+    "9941411", ["build_largest_int"]);
+assert("2B_5", () => build_2nd_largest_int([5,5,5,5,5,5,7,5,5,5]),
+    "5755555555", ["build_largest_int"]);
+assert("2B_6", () => build_2nd_largest_int([5,5,5,5,5,5,5,5,5,5]),
+    "5555555555", ["build_largest_int"]);
+
+
+//===============================================================
+// TASK 2C
+//===============================================================
+function build_nth_largest_int(digits, n) {
+
+    // WRITE HERE.
+
+}
+
+
+// TASK 2C TESTS
+assert("2C_1", () => build_nth_largest_int([1,2,4,3], 1),
+    "4321", ["build_largest_int", "build_2nd_largest_int"]);
+assert("2C_2", () => build_nth_largest_int([3,1,4,2], 2),
+    "4312", ["build_largest_int", "build_2nd_largest_int"]);
+assert("2C_3", () => build_nth_largest_int([3,1,4,2], 10),
+    "3214", ["build_largest_int", "build_2nd_largest_int"]);
+assert("2C_4", () => build_nth_largest_int([1,3,4,2], 18),
+    "2134", ["build_largest_int", "build_2nd_largest_int"]);
+assert("2C_5", () => build_nth_largest_int([3,1,4,2], 24),
+    "1234", ["build_largest_int", "build_2nd_largest_int"]);
+assert("2C_6", () => build_nth_largest_int([4,3,2,1], 28),
+    "1234", ["build_largest_int", "build_2nd_largest_int"]);
+assert("2C_7", () => build_nth_largest_int([5,3,7], 1),
+    "753", ["build_largest_int", "build_2nd_largest_int"]);
+assert("2C_8", () => build_nth_largest_int([3,5,7], 4),
+    "537", ["build_largest_int", "build_2nd_largest_int"]);
+assert("2C_9", () => build_nth_largest_int([5,3,7], 6),
+    "357", ["build_largest_int", "build_2nd_largest_int"]);
+assert("2C_10", () => build_nth_largest_int([5,3,7], 10),
+    "357", ["build_largest_int", "build_2nd_largest_int"]);
+assert("2C_11", () => build_nth_largest_int([5], 10),
+    "5", ["build_largest_int", "build_2nd_largest_int"]);
 
 
 //===============================================================
